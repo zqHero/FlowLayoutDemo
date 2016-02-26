2eab1c6afe8332daaa1325c5e8ca2efbce571f6f
zhaoqiang <229457269@qq.com>
2016-02-26 13:45:46 +0800

    流布局，中实现   标签的选中，添加和删除..

---
+++ src/com/ac/flowlayout_danxuan/TagItem.java
@@ -0,0 +1,15 @@
+package com.ac.flowlayout_danxuan;
+
+import android.widget.TextView;
+
+/**
+ * com.ac.flowlayout_danxuan
+ * @Email zhaoq_hero@163.com
+ * @author zhaoQiang : 2016-2-23
+ */
+public class TagItem {
+	public String tagText;  //标签上的文本信息：
+	public boolean tagCustomEdit;
+	public int idx;
+	public TextView mView;
+}
